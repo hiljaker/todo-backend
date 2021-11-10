4b6a42a6385d3116dfcec4641077de5a12b27908
rachmann0 <reecedarby1889@gmail.com>
2021-11-11 00:57:58 +0700

    finished deleteActivity controller

--- src/controllers/activitiesControllers.js
+++ src/controllers/activitiesControllers.js
@@ -10,7 +10,6 @@ exports.addActivity = async (req, res) => {
     act_finish: '2021-11-10 16:30:00',
   };
   const { activity_name, description, act_start, act_finish } = req.body.data;
-
   // const { activity_name, description, act_start, act_finish } = JSON.parse(
   //   req.body.data
   // );
@@ -69,18 +68,18 @@ exports.addActivity = async (req, res) => {
   }
 };
 
-// ? read
+// ? READ
 exports.getActivity = async (req, res) => {
   const { activity_name } = req.params;
   const { id } = req.user;
 
   const conn = await pool.promise().getConnection();
   try {
-    let sql = 'select * from activity where user_id = ?';
+    let sql = 'SELECT * FROM activity WHERE user_id = ?';
 
     // * get all activities if no params
     if (activity_name === undefined) sql += ';';
-    else sql += ' and activity_name = ?;';
+    else sql += ' AND activity_name = ?;';
 
     const [results] = await conn.query(sql, [id, activity_name]);
 
@@ -109,11 +108,25 @@ exports.editActivity = async (req, res) => {
 
 // ? DELETE
 exports.deleteActivity = async (req, res) => {
+  // * activity_name assumed to be unique in the database
+  const { activity_name } = req.params;
   const { id } = req.user;
+
   const conn = await pool.promise().getConnection();
   try {
+    let sql = 'DELETE FROM activity WHERE user_id = ? AND activity_name = ?;';
+    const [results] = await conn.query(sql, [id, activity_name]);
+
     conn.release();
-    return res.status(200).json({ results });
+    if (results?.affectedRows) {
+      return res
+        .status(200)
+        .json({ results: { deleted: results.affectedRows } });
+    } else {
+      return res
+        .status(400)
+        .json({ message: `no activity named '${activity_name}'` });
+    }
   } catch (error) {
     conn.release();
     console.log(error);
